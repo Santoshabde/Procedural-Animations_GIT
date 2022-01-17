6ba1a353dce977b55c24d4160d5a2db1a2f41dee
Santoshabde <65105410+Santoshabde@users.noreply.github.com>
2022-01-17 21:05:44 +0530

    Update 1.02

--- Assets/Scripts/Robot/DamagableBoxes.cs
+++ Assets/Scripts/Robot/DamagableBoxes.cs
@@ -14,7 +14,7 @@ public class DamagableBoxes : MonoBehaviour, IDamagable
         normalObj.SetActive(false);
         destructionObj.SetActive(true);
 
-        for (int i = 0; i < destructionObj.transform.GetChildCount(); i++)
+        for (int i = 0; i < destructionObj.transform.childCount; i++)
         {
             Debug.Log(destructionObj.transform.GetChild(i).name);
             destructionObj.transform.GetChild(i).GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(0f,1f), Random.Range(0f, 1f), Random.Range(0f, 1f)) * 10f, ForceMode.Impulse);
